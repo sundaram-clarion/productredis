55452eab52d9a520b54d043fac9c05387a63c957
Sundaram Talekar <sundaram.talekar@clariontechnologies.co.in>
2019-03-24 19:04:06 +0530

    Added caching using redis.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use App\Category;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -22,5 +23,12 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->company,
+        'description' => $faker->text
     ];
 });
